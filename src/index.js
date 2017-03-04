5593df6878ff96ebde6ce23d773560f44169a895
Thiago Barcala <thiago.barcala@gmail.com>
2017-03-05 00:27:38 +0100

    Refactor index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const t = require('babel-types');
 const fs = require('fs');
 const path = require('path');
 
-function getPropExpression(memberExpression) {
+const getPropExpression = memberExpression => {
     if (memberExpression.property.type === 'Identifier' && !memberExpression.computed) {
         return t.stringLiteral(memberExpression.property.name);
     }
@@ -12,118 +12,120 @@ function getPropExpression(memberExpression) {
     }
 
     return memberExpression.property;
-}
+};
+
+const prependRuntime = program => {
+    const runtime = fs.readFileSync(path.join(__dirname, 'runtime.js'));
+    const runtimeStatement = t.expressionStatement(t.identifier(runtime));
+    program.node.body.unshift(runtimeStatement);
+};
+
+const replaceSetterCall = expression => {
+    if (expression.node.left.type !== 'MemberExpression') {
+        return;
+    }
+    
+    expression.replaceWith(
+        t.callExpression(
+            t.identifier('callSetter'),
+            [
+                expression.node.left.object,
+                getPropExpression(expression.node.left),
+                expression.node.right
+            ]
+        )
+    );
+};
+
+const replaceAssignemntOperators = expression => {
+    if (expression.node.argument.type === 'CallExpression' &&
+        expression.node.argument.callee.type === 'Identifier' &&
+        expression.node.argument.callee.name === 'callGetter') {
+
+        var args = [
+            expression.node.argument.arguments[0],
+            expression.node.argument.arguments[1]
+        ];
+        expression.replaceWith(
+            t.callExpression(
+                t.identifier(expression.node.operator === '++' ? 'doIncrement' : 'doDecrement'),
+                args
+            )
+        );
+    }
+};
+
+const replaceGetterCall = expression => {
+    if (expression.node.property.type === 'Identifier' &&
+        expression.node.property.name === 'constructor') {
+        return;
+    }
+
+    if (expression.node.object.type === 'Identifier' &&
+        expression.node.property.type === 'Identifier' &&
+        expression.node.object.name === 'Object' &&
+        expression.node.property.name === 'defineProperty'
+    ) {
+        return;
+    }
+
+    if (expression.node.object.type === 'Identifier' &&
+        expression.node.property.type === 'Identifier' &&
+        expression.node.object.name === 'Object' &&
+        expression.node.property.name === 'defineProperties'
+    ) {
+        return;
+    }
+
+    expression.replaceWith(
+        t.callExpression(
+            t.identifier('callGetter'),
+            [
+                expression.node.object,
+                getPropExpression(expression.node)
+            ]
+        )
+    );
+};
+
+const replaceInvokeCall = expression => {
+    if (expression.node.callee.type === 'CallExpression' &&
+        expression.node.callee.callee.type === 'Identifier' &&
+        expression.node.callee.callee.name === 'callGetter') {
+        var args = [
+            expression.node.callee.arguments[0],
+            expression.node.callee.arguments[1],
+            t.arrayExpression(expression.node.arguments)
+        ];
+        expression.replaceWith(
+            t.callExpression(
+                t.identifier('invokeGetter'),
+                args
+            )
+        );
+    }
+};
 
 module.exports = {
     visitor: {
         Program: {
-            enter: function (program) {
-                const runtime = fs.readFileSync(path.join(__dirname, 'runtime.js'));
-                const runtimeStatement = t.expressionStatement(t.identifier(runtime));
-                program.node.body.unshift(runtimeStatement);
-            }
+            enter: prependRuntime
         },
 
         AssignmentExpression: {
-            enter: function (expression) {
-                if (expression.node.left.type === 'MemberExpression') {
-                    expression.replaceWith(
-                        t.callExpression(
-                            t.identifier('callSetter'),
-                            [
-                                expression.node.left.object,
-                                getPropExpression(expression.node.left),
-                                expression.node.right
-                            ]
-                        )
-                    );
-                }
-            }
+            enter: replaceSetterCall
         },
 
         UpdateExpression: {
-            exit: function (expression) {
-                if (expression.node.argument.type === 'CallExpression' &&
-                    expression.node.argument.callee.type === 'Identifier' &&
-                    expression.node.argument.callee.name === 'callGetter') {
-
-                    var args = [
-                        expression.node.argument.arguments[0],
-                        expression.node.argument.arguments[1]
-                    ];
-                    expression.replaceWith(
-                        t.callExpression(
-                            t.identifier(expression.node.operator === '++' ? 'doIncrement' : 'doDecrement'),
-                            args
-                        )
-                    );
-                }
-            }
+            exit: replaceAssignemntOperators
         },
 
         MemberExpression: {
-            enter: function (expression) {
-                if (expression.node.property.type === 'Identifier' &&
-                    expression.node.property.name === 'constructor') {
-                    return;
-                }
-
-                if (expression.node.object.type === 'Identifier' &&
-                    expression.node.property.type === 'Identifier' &&
-                    expression.node.object.name === 'Object' &&
-                    expression.node.property.name === 'defineProperty'
-                ) {
-                    /*
-                    expression.replaceWith(
-                        t.identifier('Object__defineProperty')
-                    );
-                    */
-                    return;
-                }
-
-                if (expression.node.object.type === 'Identifier' &&
-                    expression.node.property.type === 'Identifier' &&
-                    expression.node.object.name === 'Object' &&
-                    expression.node.property.name === 'defineProperties'
-                ) {
-                    /*
-                    expression.replaceWith(
-                        t.identifier('Object__defineProperties')
-                    );
-                    */
-                    return;
-                }
-
-                expression.replaceWith(
-                    t.callExpression(
-                        t.identifier('callGetter'),
-                        [
-                            expression.node.object,
-                            getPropExpression(expression.node)
-                        ]
-                    )
-                );
-            }
+            enter: replaceGetterCall
         },
 
         CallExpression: {
-            exit: function (expression) {
-                if (expression.node.callee.type === 'CallExpression' &&
-                    expression.node.callee.callee.type === 'Identifier' &&
-                    expression.node.callee.callee.name === 'callGetter') {
-                    var args = [
-                        expression.node.callee.arguments[0],
-                        expression.node.callee.arguments[1],
-                        t.arrayExpression(expression.node.arguments)
-                    ];
-                    expression.replaceWith(
-                        t.callExpression(
-                            t.identifier('invokeGetter'),
-                            args
-                        )
-                    );
-                }
-            }
+            exit: replaceInvokeCall
         }
     }
 };
